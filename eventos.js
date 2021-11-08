fcd351445b6537eedacb087cc8ab3c91b7a1cdc4
Santiago <santiagosalazar.dev@gmail.com>
2021-11-08 15:55:23 -0400

    Ahora tiene 4 tipos de marcas

--- eventos.js
+++ eventos.js
@@ -1,9 +1,34 @@
 var d = document.getElementById("area_de_dibujo");
 var lienzo = d.getContext("2d");
-var color = "blue";
+var pixels = 5;
+var colorMouse = "blue";
+var colorFlechas = "red";
+var colorPunto = "yellow";
+var colorLinea = "green";
+var xinicial;
+var yinicial;
+var xfinal;
+var yfinal;
 
-d.addEventListener("mousedown", activarPluma);
-d.addEventListener("mouseup", desactivarPluma);
+var teclas = {
+  LEFT: 37,
+  UP: 38,
+  RIGHT: 39,
+  DOWN: 40
+};
+
+// BOTONES
+var btn_arranque = document.getElementById("puntoDeArranque");
+var btn_trazo = document.getElementById("pluma");
+var btn_punto = document.getElementById("marcarPunto");
+var btn_recta = document.getElementById("hacerLineaRecta");
+
+// addEventListener DE BASE
+btn_arranque.addEventListener("click", activadorFlechas);
+btn_trazo.addEventListener("click", activadorPluma);
+btn_punto.addEventListener("click", activadorPunto);
+btn_recta.addEventListener("click", activadorRecta);
+
 
 function dibujante(color, xinicial, yinicial, xfinal, yfinal)
 {
@@ -17,23 +42,67 @@ function dibujante(color, xinicial, yinicial, xfinal, yfinal)
   lienzo.closePath();
 }
 
+// ACTIVADORES
+function activadorFlechas()
+{
+  d.addEventListener("mouseup", marcar);
+  document.addEventListener("keydown", calcular);
+  d.removeEventListener("mousedown", activarPluma);
+  d.removeEventListener("mouseup", desactivarPluma);
+  d.removeEventListener("mouseup", punto);
+  d.removeEventListener("mousedown", punto_A);
+  d.removeEventListener("mouseup", punto_B);
+}
+
+function activadorPluma()
+{
+  d.addEventListener("mousedown", activarPluma);
+  d.addEventListener("mouseup", desactivarPluma);
+  d.removeEventListener("mouseup", marcar);
+  document.removeEventListener("keydown", calcular);
+  d.removeEventListener("mouseup", punto);
+  d.removeEventListener("mousedown", punto_A);
+  d.removeEventListener("mouseup", punto_B);
+}
+
+function activadorPunto()
+{
+  d.addEventListener("mouseup", punto);
+  d.removeEventListener("mousedown", activarPluma);
+  d.removeEventListener("mouseup", desactivarPluma);
+  d.removeEventListener("mouseup", marcar);
+  document.removeEventListener("keydown", calcular);
+  d.removeEventListener("mousedown", punto_A);
+  d.removeEventListener("mouseup", punto_B);
+}
+function activadorRecta()
+{
+  d.addEventListener("mousedown", punto_A);
+  d.addEventListener("mouseup", punto_B);
+  d.removeEventListener("mousedown", activarPluma);
+  d.removeEventListener("mouseup", desactivarPluma);
+  d.removeEventListener("mouseup", marcar);
+  document.removeEventListener("keydown", calcular);
+  d.removeEventListener("mouseup", punto);
+}
+
+// FUNCIONES "ACTIVADAS"
+function marcar(event)
+{
+  // verificando donde se hizo el click respecto al documento
+  var a = event.clientX;
+  var b = event.clientY;
+  // verificando donde esta el elemento clickado en referencia al documento
+  var c = d.offsetLeft;
+  var e = d.offsetTop;
+  // Establecer las coordenadas que se usaran en la funcion que dibujara la linea
+  xinicial = a - c;
+  yinicial = b - e;
+  xfinal = xinicial;
+  yfinal = yinicial;
+}
 
 // A PARTIR DE ACA ES PINTAR CON LAS TECLAS
-var teclas = {
-  LEFT: 37,
-  UP: 38,
-  RIGHT: 39,
-  DOWN: 40
-};
-var pixels = 5;
-var colorFlechas = "red";
-var xinicial = 150;
-var yinicial = 150;
-var xfinal = 150;
-var yfinal = 150;
-
-document.addEventListener("keydown", calcular);
-
 function calcular(evento)
 {
   switch (evento.keyCode)
@@ -57,10 +126,10 @@ function calcular(evento)
   }
 
   dibujante(colorFlechas, xinicial, yinicial, xfinal, yfinal);
+
   xinicial = xfinal;
   yinicial = yfinal;
 }
-
 
 // A PARTIR DE ACA ES PINTAR CON EL MOUSE
 function activarPluma(event)
@@ -92,8 +161,8 @@ function trazar(event)
   // xPuntoB = xPuntoA;
   // yPuntoB = yPuntoA;
 
-  dibujante(color, --xPuntoA, --yPuntoA, ++xPuntoB, ++yPuntoB);
-  dibujante(color, --xPuntoA, ++yPuntoA, ++xPuntoB, --yPuntoB);
+  dibujante(colorMouse, --xPuntoA, --yPuntoA, ++xPuntoB, ++yPuntoB);
+  dibujante(colorMouse, --xPuntoA, ++yPuntoA, ++xPuntoB, --yPuntoB);
 
   xPuntoA = xPuntoB
   yPuntoA = yPuntoB
@@ -102,4 +171,50 @@ function trazar(event)
 function desactivarPluma(event)
 {
   d.removeEventListener("mousemove", trazar);
+}
+
+function punto(event)
+{
+  // verificando donde se hizo el click respecto al documento
+  var mousePresionadoX = event.clientX;
+  var mousePresionadoY = event.clientY;
+  // verificando donde esta el elemento clickado en referencia al documento
+  var marginLeft = d.offsetLeft;
+  var marginTop = d.offsetTop;
+  // Establecer las coordenadas que se usaran en la funcion que dibujara la linea
+  xPuntoA = mousePresionadoX - marginLeft;
+  yPuntoA = mousePresionadoY - marginTop;
+
+  dibujante(colorPunto, --xPuntoA, --yPuntoA, ++xPuntoA, ++yPuntoA);
+  dibujante(colorPunto, --xPuntoA, ++yPuntoA, ++xPuntoA, --yPuntoA);
+  dibujante(colorPunto, --xPuntoA, yPuntoA, ++xPuntoA, yPuntoA);
+}
+
+function punto_A(event)
+{
+  // verificando donde se hizo el click respecto al documento
+  var mousePresionadoX = event.clientX;
+  var mousePresionadoY = event.clientY;
+  // verificando donde esta el elemento clickado en referencia al documento
+  var marginLeft = d.offsetLeft;
+  var marginTop = d.offsetTop;
+  // Establecer las coordenadas que se usaran en la funcion que dibujara la linea
+  xPunto_A = mousePresionadoX - marginLeft;
+  yPunto_A = mousePresionadoY - marginTop;
+}
+
+function punto_B(event)
+{
+  // verificando donde se hizo el click respecto al documento
+  var mousePresionadoX = event.clientX;
+  var mousePresionadoY = event.clientY;
+  // verificando donde esta el elemento clickado en referencia al documento
+  var marginLeft = d.offsetLeft;
+  var marginTop = d.offsetTop;
+  // Establecer las coordenadas que se usaran en la funcion que dibujara la linea
+  xPunto_B = mousePresionadoX - marginLeft;
+  yPunto_B = mousePresionadoY - marginTop;
+
+  dibujante(colorLinea, --xPunto_A, --yPunto_A, ++xPunto_B, ++yPunto_B);
+  dibujante(colorLinea, --xPunto_A, ++yPunto_A, ++xPunto_B, --yPunto_B);
 }
